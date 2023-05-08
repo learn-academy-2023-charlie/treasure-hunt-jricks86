8d49df72b9127752e578fb049cb8dd2463d8923c
jricks86 <jricks86@gmail.com>
2023-05-08 13:40:58 -0700

    tree emoji

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import "./App.css"
+import Square from "./components/Square"
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -14,9 +15,14 @@ const App = () => {
     "?"
   ])
 
+const handleClick = (selectedIndex) => {
+  alert(selectedIndex)
+}
+
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <Square board={board} handleClick={handleClick}/>
     </>
   )
 }
